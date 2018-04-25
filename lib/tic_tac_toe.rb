2d4c11e36faef29fef89e87ddff3720d0452d9f1
Kristopher Limpus <krislimpus@gmail.com>
2018-04-25 22:26:11 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -34,8 +34,8 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
-def move(index, value = "X")
-  @board[index] = value
+def move(index, player = "X")
+  @board[index] = player
 end
 
 def position_taken?(index)
@@ -50,11 +50,69 @@ def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
 end
 
-def turn(index)
+def turn
   puts "Please enter 1-9:"
   input = gets.strip
-  
+  index = input_to_index(input)
+  player = current_player
+  if valid_move?(index)
+    move(index, player)
+    display_board
+  else
+    turn
+  end
 end
+
+def won?
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+
+    position_1 = @board[win_index_1]
+    position_2 = @board[win_index_2]
+    position_3 = @board[win_index_3]
+
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" || position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
+    end
+
+  end
+else
+  false
+end
+
+def full?
+  !@board.any? { |x| x == " " }
+end
+
+def draw?
+  if !won? && full?
+    return true
+  else
+    return false
+  end
+end
+
+def over?
+  if won? || draw? || full?
+    return true
+  else
+    return false
+  end
+end
+
+def winner
+  winning_board = won?
+ if winning_board == false
+   return nil
+ elsif @board[winning_board[0]].include?("X")
+   "X"
+ else @board[winning_board[0]].include?("O")
+   "O"
+ end
+end
+
 
 
 end
